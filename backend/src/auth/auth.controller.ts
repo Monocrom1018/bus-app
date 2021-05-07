292b565ea1dc96bd3b190b91a6fe463b0504ce11
insomenia-woobyeong <woobyeong@insomenia.com>
2021-05-07 15:29:07 +0900

    [#2] edit auth controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -48,6 +48,7 @@ export class AuthController {
     @Body() authCredentialsDto: AuthCredentialsDto,
     @UploadedFile() file: Express.Multer.File,
   ) {
+    console.log(authCredentialsDto);
     const csrfToken = randomBytes(32).toString('base64');
     const accessToken = await this.authService.login(authCredentialsDto);
 
