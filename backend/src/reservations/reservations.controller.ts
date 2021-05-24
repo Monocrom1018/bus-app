b1f2d98c356c959163a0ef4bf824ab8dad9bff46
LeeSeungBae <seungbae@insomenia.com>
2021-05-24 15:37:39 +0900

    [#19] midify search page UI @2

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -49,22 +49,21 @@ export class ReservationsController {
     destCoord.x = destinationData.data.addresses[0].x;
     destCoord.y = destinationData.data.addresses[0].y;
 
-    const distanceData = await axios.get(
-      `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${depCoord.x},${depCoord.y}&goal=${destCoord.x},${destCoord.y}`,
-      {
-        headers: {
-          'X-NCP-APIGW-API-KEY-ID': process.env.X_NCP_APIGW_API_KEY_ID,
-          'X-NCP-APIGW-API-KEY': process.env.X_NCP_APIGW_API_KEY,
-        },
-      },
-    );
+    //! 아래는 이용 시 요금 부과되는 서비스라 주석처리 해뒀습니다. 주석해제 시 km거리 산출됩니다.
+    // const distanceData = await axios.get(
+    //   `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${depCoord.x},${depCoord.y}&goal=${destCoord.x},${destCoord.y}`,
+    //   {
+    //     headers: {
+    //       'X-NCP-APIGW-API-KEY-ID': process.env.X_NCP_APIGW_API_KEY_ID,
+    //       'X-NCP-APIGW-API-KEY': process.env.X_NCP_APIGW_API_KEY,
+    //     },
+    //   },
+    // );
 
-    const kmData = (
-      distanceData.data.route.traoptimal[0].summary.distance / 1000
-    ).toFixed(1);
+    // const kmData = (
+    //   distanceData.data.route.traoptimal[0].summary.distance / 1000
+    // ).toFixed(1);
 
-    console.log(kmData + 'km');
-
-    return kmData;
+    return '37.5';
   }
 }
