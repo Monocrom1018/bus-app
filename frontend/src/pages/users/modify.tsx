325066066011bf1b9d62842fde268f000ed84241
LeeSeungBae <seungbae@insomenia.com>
2021-06-01 11:02:54 +0900

    add profile image upload & modify address search UI

--- frontend/src/pages/users/modify.tsx
+++ frontend/src/pages/users/modify.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { f7, Navbar, Page, List, ListInput, Button } from 'framework7-react';
-import { Formik, Form } from 'formik';
+import { convertObjectToFormData, sleep } from '@utils';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import DaumAddressSearch from '@components/shared/DaumAddressSearch';
 
 const UserInfoSchema = Yup.object().shape({
   password: Yup.string(),
   password_confirmation: Yup.string(),
+  profile_img: Yup.mixed().required('A file is required'),
 });
 
 const ModifyPage = () => {
+  const [imgState, setImgState] = useState({ file: '', previewURL: null });
+
+  const handleImgButton = () => {
+    document.getElementById('imageInput').click();
+  };
+
+  const handleFileOnChange = async (event) => {
+    event.preventDefault();
+
+    if (event.target.files[0]) {
+      let reader = new FileReader();
+      let file = event.target.files[0];
+
+      reader.onloadend = () => {
+        setImgState({
+          file: file,
+          previewURL: reader.result,
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <Page noToolbar>
       {/* Top Navbar */}
@@ -24,15 +49,43 @@ const ModifyPage = () => {
           zipcode: '',
           address1: '',
           address2: '',
+          profile_img: '',
         }}
         validationSchema={UserInfoSchema}
-        onSubmit={async (values, { setSubmitting }) => {}}
+        onSubmit={async (values, { setSubmitting }) => {
+          await sleep(400);
+          setSubmitting(false);
+          f7.dialog.preloader('잠시만 기다려주세요...');
+          try {
+            console.log(values);
+            // const fd = convertObjectToFormData({ modelName: 'user', data: values });
+            // const response = await modifyAPI(fd);
+            f7.dialog.close();
+          } catch (error) {
+            f7.dialog.close();
+            f7.dialog.alert(error?.response?.data || error?.message);
+          }
+        }}
         validateOnMount={true}
       >
-        {({ handleChange, handleBlur, values, errors, touched, isSubmitting, isValid }) => (
-          <Form>
+        {({ setFieldValue, handleChange, handleBlur, values, errors, touched, isSubmitting, isValid }) => (
+          <Form encType="multipart/form-data">
             <List noHairlinesMd>
               <div className="p-3 font-semibold bg-white">기본 정보</div>
+              <div className="flex flex-col items-center bg-white border-t">
+                <img src={imgState.previewURL} className="rounded-3xl mt-4 w-36 h-36 object-cover"></img>
+                <Button className="my-2 font-semibold" onClick={handleImgButton}>
+                  프로필사진 변경
+                </Button>
+                <input
+                  id="imageInput"
+                  type="file"
+                  accept="image/jpg,impge/png,image/jpeg,image/gif"
+                  name="profile_img"
+                  onChange={handleFileOnChange}
+                  className="hidden"
+                ></input>
+              </div>
               <ListInput disabled outline label={i18next.t('login.name')} type="text" name="name" value="홍길동" />
               <ListInput
                 disabled
@@ -63,9 +116,15 @@ const ModifyPage = () => {
                 errorMessage={touched.password_confirmation && errors.password_confirmation}
               />
             </List>
-
             <DaumAddressSearch />
 
+            {/* <input
+              type="file"
+              name="profile_img"
+              accept="image/jpg, image/png, image/jpeg"
+              onChange={(e) => setFieldValue('profile_img', e.currentTarget.files[0])}
+              className="p-4"
+            /> */}
             <div className="p-4">
               <button
                 type="submit"
