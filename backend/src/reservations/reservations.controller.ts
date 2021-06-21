3088d85f8d92d7c417072c7be5374a9ca8c12e71
insomenia-woobyeong <woobyeong@insomenia.com>
2021-06-21 10:14:33 +0900

    [#56] add swagger setting

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ReservationsService } from './reservations.service';
 
+@ApiTags('예약')
 @Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
 
+  @ApiOperation({ summary: '예약 생성' })
   @Post('create')
   async create(@Body() params) {
     return this.reservationsService.create(params);
