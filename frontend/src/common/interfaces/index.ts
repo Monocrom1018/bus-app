227728912d3f2bc9277e3810b2db763781981ba4
LeeSeungBae <seungbae@insomenia.com>
2021-07-29 18:45:52 +0900

    schedule decide button add, get distance and show

--- frontend/src/common/interfaces/index.ts
+++ frontend/src/common/interfaces/index.ts
@@ -138,6 +138,7 @@ export interface Schedule {
   landing: string;
   landingState: boolean;
   returnStopOverCheck: boolean;
+  distance: number;
   pointList: any;
   preStopOvers: StopOver[];
   postStopOvers: StopOver[];
